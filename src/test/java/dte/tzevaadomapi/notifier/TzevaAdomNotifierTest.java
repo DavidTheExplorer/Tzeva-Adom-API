0d0098e98ecb33b6384dbeb69869bc79026eb4c5
David <darknight4123@gmail.com>
2023-04-09 22:00:34 +0300

    Fixed Mockito being incompatible with Java 8

--- src/test/java/dte/tzevaadomapi/notifier/TzevaAdomNotifierTest.java
+++ src/test/java/dte/tzevaadomapi/notifier/TzevaAdomNotifierTest.java
@@ -2,7 +2,6 @@ package dte.tzevaadomapi.notifier;
 
 import static dte.tzevaadomapi.utils.UncheckedExceptions.unchecked;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.time.Duration;
@@ -10,45 +9,42 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import dte.tzevaadomapi.alert.Alert;
 import dte.tzevaadomapi.alertsource.AlertSource;
 
 @TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 public class TzevaAdomNotifierTest
 {
-	private LocalDateTime now;
+	@Mock
 	private AlertSource alertSource;
 	
 	private static final Duration SIMULATION_DURATION = Duration.ofMillis(200);
-	
-	@BeforeAll
-	public void setup() throws Exception 
-	{
-		this.now = LocalDateTime.now();
-		this.alertSource = mock(AlertSource.class);
-	}
 
 	@Test
 	public void testNotTzevaAdom() throws Exception 
 	{
-		Alert alert = new Alert("Tel Aviv", this.now);
+		Alert alert = new Alert("Tel Aviv", LocalDateTime.now());
 		when(this.alertSource.getMostRecentAlert()).thenReturn(alert, alert, alert);
-		assertHistoryEquals(simulateNotifier(this.alertSource), alert);
+		
+		assertHistoryEquals(simulateNotifier(), alert);
 	}
 	
 	@Test
 	public void testTzevaAdomDetection() throws Exception
 	{
-		Alert firstAlert = new Alert("Tel Aviv", this.now);
-		Alert secondAlert = new Alert("Haifa", this.now);
-		
+		Alert firstAlert = new Alert("Tel Aviv", LocalDateTime.now());
+		Alert secondAlert = new Alert("Haifa", LocalDateTime.now());
 		when(this.alertSource.getMostRecentAlert()).thenReturn(firstAlert, firstAlert, firstAlert, firstAlert, secondAlert);
-		assertHistoryEquals(simulateNotifier(this.alertSource), firstAlert, secondAlert);
+		
+		assertHistoryEquals(simulateNotifier(), firstAlert, secondAlert);
 	}
 	
 	
@@ -57,11 +53,17 @@ public class TzevaAdomNotifierTest
 		assertIterableEquals(notifier.getHistory(), Arrays.asList(expectedHistory));
 	}
 	
-	private TzevaAdomNotifier simulateNotifier(AlertSource alertSource) throws InterruptedException 
+	/**
+	 * Runs a notifier(based on the mocked {@code alertSource}) for the defined <b>Simulation Duration</b>, and then returns it.
+	 * 
+	 * @return A notifier after a simulated run.
+	 * @throws InterruptedException when the sleeping between requests fails.
+	 */
+	private TzevaAdomNotifier simulateNotifier() throws InterruptedException 
 	{
 		TzevaAdomNotifier notifier = new TzevaAdomNotifier.Builder()
 				.every(Duration.ofMillis(5))
-				.requestFrom(alertSource)
+				.requestFrom(this.alertSource)
 				.onFailedRequest(Exception::printStackTrace)
 				.build();
 		
