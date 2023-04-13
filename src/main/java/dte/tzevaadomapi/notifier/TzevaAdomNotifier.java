28985473e94d9fca21b64fc8692aadff1544fc30
David <darknight4123@gmail.com>
2023-04-13 04:23:08 +0300

    Added TzevaAdomListener to replace Consumer<Alert>
    This simplifies both client code & the experience of the developers reading the library.

--- src/main/java/dte/tzevaadomapi/notifier/TzevaAdomNotifier.java
+++ src/main/java/dte/tzevaadomapi/notifier/TzevaAdomNotifier.java
@@ -29,7 +29,7 @@ public class TzevaAdomNotifier implements Iterable<Alert>
 	private final AlertSource alertSource;
 	private final Duration requestDelay;
 	private final Consumer<Exception> requestFailureHandler;
-	private final Set<Consumer<Alert>> listeners = new HashSet<>();
+	private final Set<TzevaAdomListener> listeners = new HashSet<>();
 	private final Deque<Alert> history = new LinkedList<>();
 
 	private TzevaAdomNotifier(AlertSource alertSource, Duration requestDelay, Consumer<Exception> requestFailureHandler) 
@@ -66,15 +66,15 @@ public class TzevaAdomNotifier implements Iterable<Alert>
 				
 				lastTzevaAdom = alert;
 				
-				this.listeners.forEach(listener -> listener.accept(alert));
+				this.listeners.forEach(listener -> listener.onTzevaAdom(alert));
 				this.history.add(alert);
 			}
 		});
 	}
 
-	public void addListener(Consumer<Alert> tzevaAdomListener) 
+	public void addListener(TzevaAdomListener listener) 
 	{
-		this.listeners.add(tzevaAdomListener);
+		this.listeners.add(listener);
 	}
 
 	public Alert getLastAlert()
@@ -117,7 +117,7 @@ public class TzevaAdomNotifier implements Iterable<Alert>
 		private AlertSource alertSource;
 		private Duration requestDelay;
 		private Consumer<Exception> requestFailureHandler = (exception) -> {};
-		private Set<Consumer<Alert>> listeners = new HashSet<>();
+		private Set<TzevaAdomListener> listeners = new HashSet<>();
 
 		public Builder requestFrom(AlertSource alertSource) 
 		{
@@ -137,7 +137,7 @@ public class TzevaAdomNotifier implements Iterable<Alert>
 			return this;
 		}
 		
-		public Builder onTzevaAdom(Consumer<Alert> listener)
+		public Builder onTzevaAdom(TzevaAdomListener listener)
 		{
 			this.listeners.add(listener);
 			return this;
