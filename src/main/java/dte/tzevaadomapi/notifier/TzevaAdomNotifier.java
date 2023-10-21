8e3fe99233dd37b5f63d7cb4ed61da9105e79f2c
David <darknight4123@gmail.com>
2023-10-21 10:45:02 +0300

    TzevaAdomNotifier#getLastAlert now returns an Optional
    Empty one means there were no Tzeva Adoms since the notifier was started.

--- src/main/java/dte/tzevaadomapi/notifier/TzevaAdomNotifier.java
+++ src/main/java/dte/tzevaadomapi/notifier/TzevaAdomNotifier.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -76,9 +77,9 @@ public class TzevaAdomNotifier
 		this.listeners.add(listener);
 	}
 
-	public Alert getLastAlert()
+	public Optional<Alert> getLastAlert()
 	{
-		return this.history.peekLast();
+		return Optional.ofNullable(this.history.peekLast());
 	}
 
 	public Deque<Alert> getHistory()
