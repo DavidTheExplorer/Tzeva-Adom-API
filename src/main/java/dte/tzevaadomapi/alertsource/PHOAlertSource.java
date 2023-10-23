cb76e962f1eefa8724514e30ad4d045b035137d4
David <darknight4123@gmail.com>
2023-10-23 15:23:23 +0300

    Merged JSONAlertSource into PHOAlertSource
    The abstract class turned out to be useless, and also having just a single alert source(Pikud Ha'oref) makes the library more clear.

--- src/main/java/dte/tzevaadomapi/alertsource/PHOAlertSource.java
+++ src/main/java/dte/tzevaadomapi/alertsource/PHOAlertSource.java
@@ -4,25 +4,51 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 import dte.tzevaadomapi.alert.Alert;
-import dte.tzevaadomapi.alertsource.json.JSONAlertSource;
 
 /**
  * Requests Alerts from the the website of Pikud Ha Oref.
  */
-public class PHOAlertSource extends JSONAlertSource
+public class PHOAlertSource implements AlertSource
 {
-	public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+	private static final String REQUEST_URL = "https://www.oref.org.il/WarningMessages/History/AlertsHistory.json";
 	
-	public PHOAlertSource()
-	{
-		super("https://www.oref.org.il/WarningMessages/History/AlertsHistory.json");
-	}
+	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
 	
 	@Override
-	protected Alert fromJSON(JSONObject alertJson) 
+	public Alert getMostRecentAlert() throws Exception
+	{
+		JSONArray alertsJsonArray = requestAlertsJSON();
+		
+		if(alertsJsonArray == null || alertsJsonArray.isEmpty())
+			return NO_RESPONSE;
+		
+		return parseAlert((JSONObject) alertsJsonArray.get(0));
+	}
+	
+	private JSONArray requestAlertsJSON() throws Exception
+	{
+		try(CloseableHttpClient httpClient = HttpClients.createDefault();
+				CloseableHttpResponse response = httpClient.execute(new HttpGet(REQUEST_URL))) 
+		{
+			HttpEntity entity = response.getEntity();
+			String alertsJsonText = (entity != null ? EntityUtils.toString(entity) : null);
+			
+			return (JSONArray) JSONValue.parse(alertsJsonText);
+		}
+	}
+	
+	private Alert parseAlert(JSONObject alertJson) 
 	{
 		String city = (String) alertJson.get("data");
 		String title = (String) alertJson.get("title");
